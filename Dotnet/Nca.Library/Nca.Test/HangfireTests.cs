2c10cd1cc8e4386c754b01dff7ea5e79b56a6ef7
An Nguyen Cao <anguyencao@win.ips-ag.net>
2019-04-17 14:18:55 +0700

    up date code

--- Dotnet/Nca.Library/Nca.Test/HangfireTests.cs
+++ Dotnet/Nca.Library/Nca.Test/HangfireTests.cs
@@ -6,29 +6,35 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Nca.Test.Jobs;
 using NUnit.Framework;
+using ServiceStack.Messaging.Redis;
+using ServiceStack.Redis;
 using StackExchange.Redis;
 using System;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Tests
 {
     public class HangfireTests : IDisposable
     {
-        public ConnectionMultiplexer Redis;
-        private IWebHost host;
+        private ConnectionMultiplexer _redis;
+        private IWebHost _host;
         [SetUp]
         public void Setup()
         {
-            Redis = ConnectionMultiplexer.Connect("localhost:6379");
+            _redis = ConnectionMultiplexer.Connect("localhost:6379");
+
             var hostBuilder = new WebHostBuilder();
             hostBuilder.ConfigureServices(services => ConfigureServices(services));
             hostBuilder.Configure(app => Configure(app));
             hostBuilder.UseContentRoot(Directory.GetCurrentDirectory());
             hostBuilder.UseKestrel();
-            host = hostBuilder.Build();
-
-            host.Start();
+            hostBuilder.UseUrls("http://localhost:5051");
+            _host = hostBuilder.Build();
+            _host.Start();
         }
 
 
@@ -36,7 +42,7 @@ namespace Tests
         {
             services.AddHangfire(configuration =>
             {
-                configuration.UseRedisStorage(Redis, new RedisStorageOptions { Prefix = "{hangfire-1}:" });
+                configuration.UseRedisStorage(_redis, new RedisStorageOptions { Prefix = "{hangfire-1}:" });
             });
 
             services.AddTransient<MyHangfireJobs>();
@@ -55,7 +61,9 @@ namespace Tests
             Assert.True(true);
 
             RecurringJob.AddOrUpdate(
-                    () => Console.WriteLine("Recurring!"),
+                    () =>
+
+                    Trace.WriteLine("Recurring!"),
                     Cron.Minutely());
 
 
@@ -65,7 +73,68 @@ namespace Tests
 
         public void Dispose()
         {
-            this.host.Dispose();
+            _host.Dispose();
+            _redis.Dispose();
         }
+
+        [Test]
+        public void TestRedisPubSub()
+        {
+            var channelName = "channelName";
+
+            var database = _redis.GetDatabase();
+            var subscriber = _redis.GetSubscriber();
+            subscriber.Subscribe(channelName, (channel, message) =>
+            {
+                Trace.WriteLine($"1: {DateTime.Now.ToString("yyyyMMdd HH:mm:ss")}<Normal - {channel}><{message}>.");
+            });
+
+            var subscriber2 = _redis.GetSubscriber();
+            subscriber2.Subscribe(channelName, (channel, message) =>
+            {
+                Trace.WriteLine($"2: {DateTime.Now.ToString("yyyyMMdd HH:mm:ss")}<Normal - {channel}><{message}>.");
+            });
+
+
+            var publisher = _redis.GetSubscriber();
+
+            // publish message to one channel
+            var results = publisher.Publish(channelName, $"Publish a message to literal channel: {channelName}", CommandFlags.FireAndForget);
+
+
+            Assert.True(results > 0);
+        }
+
+        public class Hello { public string Name { get; set; } }
+        public class HelloResponse { public string Result { get; set; } }
+        [Test]
+        public void TestMQRedis()
+        {
+            var redisFactory = new PooledRedisClientManager("localhost:6379");
+            var mqHost = new RedisMqServer(redisFactory, retryCount: 2);
+
+            //Server - MQ Service Impl:
+            mqHost.RegisterHandler<Hello>(m =>
+            {
+                Trace.WriteLine("Received 1: " + m.GetBody());
+                return null;
+            });
+            //Client - Process Response:
+            mqHost.RegisterHandler<HelloResponse>(m =>
+            {
+                Trace.WriteLine($"Received 2: {m.GetBody().Result}: {DateTime.Now.ToString()}");
+                return null;
+            });
+            mqHost.Start();
+
+            //Producer - Start publishing messages:
+            var mqClient = mqHost.CreateMessageQueueClient();
+            mqClient.Publish(new Hello { Name = "ServiceStack" });
+
+
+            Thread.Sleep(10000);
+            Assert.True(true);
+        }
+
     }
 }
