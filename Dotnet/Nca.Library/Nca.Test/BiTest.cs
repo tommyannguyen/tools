a9b795c98796c821964b0b4ed4017e288a62b5fe
An Nguyen Cao <anguyencao@win.ips-ag.net>
2019-05-14 16:52:30 +0700

    - update code

---
+++ Dotnet/Nca.Library/Nca.Test/BiTest.cs
@@ -0,0 +1,48 @@
+﻿using Nca.WebApp.Models;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nca.Test
+{
+    public class BiTest
+    {
+        [Test]
+        public async Task TestLogin()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var tenantId = "1a3a4261-a577-48ec-beb1-71cd3a14e413";
+                var tokenEndpoint = $"https://login.microsoftonline.com/{tenantId}/oauth2/token";
+                var accept = "application/json";
+                var userName = "adam.davis@ariaresorts.co.uk";
+                var password = "*";
+                var clientId = "68fcad01-31ed-4fe6-b280-73305c3f6e68";
+                client.DefaultRequestHeaders.Add("Accept", accept);
+
+                var content = new FormUrlEncodedContent(new[]
+                   {
+                      new KeyValuePair<string, string>("grant_type", "password"),
+                      new KeyValuePair<string, string>("username", userName),
+                      new KeyValuePair<string, string>("password", password),
+                      new KeyValuePair<string, string>("client_id", clientId),
+                      new KeyValuePair<string, string>("client_secret", "[Tf8m2/@4-m4.lFcsza.JrOD9snjBXm@"),
+                      new KeyValuePair<string, string>("scope", "openid"),
+                      new KeyValuePair<string, string>("resource", "https://analysis.windows.net/powerbi/api")
+                   });
+
+                var tokenResult = await client.PostAsync(tokenEndpoint, content).ContinueWith<string>((response) =>
+                {
+                    var result = response.Result.Content.ReadAsStringAsync().Result;
+                    AzureAdTokenResponse tokenRes = JsonConvert.DeserializeObject<AzureAdTokenResponse>(result);
+                    return tokenRes?.AccessToken;
+                });
+                Assert.True(!string.IsNullOrEmpty(tokenResult));
+            }
+        }
+    }
+}
---
+++ Dotnet/Nca.Library/Nca.Web.Spa/ClientApp/src/app/@core/mock/users.service.ts
@@ -0,0 +1,53 @@
+import { of as observableOf,  Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Contacts, RecentUsers, UserData } from '../data/users';
+
+@Injectable()
+export class UserService extends UserData {
+
+  private time: Date = new Date;
+
+  private users = {
+    nick: { name: 'Adam Davis', picture: 'assets/images/nick.png' },
+    eva: { name: 'An Nguyễn', picture: 'assets/images/eva.png' },
+    jack: { name: 'Nghi Nguyen', picture: 'assets/images/lee.png' },
+    lee: { name: 'Khiem Nguyen', picture: 'assets/images/jack.png' },
+    alan: { name: 'Adam Davis', picture: 'assets/images/nick.png' },
+    kate: { name: 'Adam Davis', picture: 'assets/images/nick.png' }
+  };
+  private types = {
+    mobile: 'mobile',
+    home: 'home',
+    work: 'work',
+  };
+  private contacts: Contacts[] = [
+    { user: this.users.nick, type: this.types.mobile },
+    { user: this.users.eva, type: this.types.home },
+    { user: this.users.jack, type: this.types.mobile },
+    { user: this.users.lee, type: this.types.mobile },
+    { user: this.users.alan, type: this.types.home },
+    { user: this.users.kate, type: this.types.work },
+  ];
+  private recentUsers: RecentUsers[]  = [
+    { user: this.users.alan, type: this.types.home, time: this.time.setHours(21, 12)},
+    { user: this.users.eva, type: this.types.home, time: this.time.setHours(17, 45)},
+    { user: this.users.nick, type: this.types.mobile, time: this.time.setHours(5, 29)},
+    { user: this.users.lee, type: this.types.mobile, time: this.time.setHours(11, 24)},
+    { user: this.users.jack, type: this.types.mobile, time: this.time.setHours(10, 45)},
+    { user: this.users.kate, type: this.types.work, time: this.time.setHours(9, 42)},
+    { user: this.users.kate, type: this.types.work, time: this.time.setHours(9, 31)},
+    { user: this.users.jack, type: this.types.mobile, time: this.time.setHours(8, 0)},
+  ];
+
+  getUsers(): Observable<any> {
+    return observableOf(this.users);
+  }
+
+  getContacts(): Observable<Contacts[]> {
+    return observableOf(this.contacts);
+  }
+
+  getRecentUsers(): Observable<RecentUsers[]> {
+    return observableOf(this.recentUsers);
+  }
+}
