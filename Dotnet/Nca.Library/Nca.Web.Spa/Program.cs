2e991cade3ef45654f7640bc2da8ded7382abfcb
An Nguyen Cao <anguyencao@win.ips-ag.net>
2019-05-14 17:31:05 +0700

    - upload configuration

--- Dotnet/Nca.Library/Nca.Web.Spa/Program.cs
+++ Dotnet/Nca.Library/Nca.Web.Spa/Program.cs
@@ -19,6 +19,8 @@ namespace Nca.Web.Spa
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .UseIISIntegration();
     }
 }
