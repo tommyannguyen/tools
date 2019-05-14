379d3d3368ab432412d5644c753f4440a75223ea
An Nguyen Cao <anguyencao@win.ips-ag.net>
2019-05-14 16:57:18 +0700

    -update code

--- Dotnet/Nca.Library/Nca.Web.Spa/ClientApp/src/app/@theme/components/footer/footer.component.ts
+++ Dotnet/Nca.Library/Nca.Web.Spa/ClientApp/src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created by nca</span>
+    <span class="created-by">Created by Nxify 2019</span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
