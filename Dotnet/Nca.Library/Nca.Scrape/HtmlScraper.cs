96453263943aa951dc4371c9190b84ca6c85573c
An Nguyen Cao <anguyencao@win.ips-ag.net>
2019-08-19 13:59:42 +0700

    - update parser

---
+++ Dotnet/Nca.Library/Nca.Scrape/HtmlScraper.cs
@@ -0,0 +1,25 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nca.Scrape
+{
+    public class HtmlScraper
+    {
+        private readonly string _url;
+
+        public HtmlScraper(string url)
+        {
+            _url = url;
+        }
+        public async Task ParseAsync()
+        {
+            var web = new HtmlWeb();
+            var document = await web.LoadFromWebAsync(_url);
+            var mainContainer = document.DocumentNode.QuerySelectorAll("#ctl31_BodyContainer").FirstOrDefault();
+
+        }
+    }
+}
---
+++ Dotnet/Nca.Library/Nca.Test/ScrapeTest.cs
@@ -0,0 +1,18 @@
+﻿using Nca.Scrape;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class ScrapeTest
+    {
+
+        [Test]
+        public async Task TestCrape()
+        {
+            var parser = new HtmlScraper("https://batdongsan.com.vn/");
+            await parser.ParseAsync();
+            Assert.True(true);
+        }
+    }
+}
